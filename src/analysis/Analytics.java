94fcf377d851ef66ec26f46a500001fe348758ce
Elijah Bassett <Bassett@@unc.edu>
2021-03-14 20:42:36 -0400

    Fixed counter misalignment issue.

--- src/analysis/Analytics.java
+++ src/analysis/Analytics.java
@@ -3,20 +3,16 @@ package analysis;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Analytics {
     public static void main(String[] args) {
         //TODO: For now stock history must be preloaded, need to fetch automatically from online API.
-        Stock AVEO = new StockImpl("src/analysis/data/AVEO.csv");
-        Stock TSLA = new StockImpl("src/analysis/data/TSLA.csv");
         comparePrediction("src/analysis/data/Futures.csv");
     }
 
     public static void comparePrediction(String path) {
-
-        // Create scanner object if file exists. Otherwise, die.
+        // Create scanner line object if file exists. Otherwise, die.
         Scanner csvReader = null;
         try {
             csvReader = new Scanner(new File(path));
@@ -24,43 +20,45 @@ public class Analytics {
             e.printStackTrace();
         }
 
-        // Initialize temporary ArrayLists.
-        ArrayList<String> tempRow = new ArrayList<String>();
-        ArrayList<Stock> tempTickers = new ArrayList<Stock>();
-
-        int s = -1;
-        int i = 0;
+        // Initialize temporary ArrayList and local variables.
+        ArrayList<Stock> tempTickers = new ArrayList<>();
+        int n = 0; // Row number. Resets for each firm.
+        int i = 0; // Cell number. Resets each row.
         String controlDate = null;
         String workingDate = null;
+
         while (csvReader.hasNextLine()) {
-            // Create new scanner to traverse each line.
+            // Create new scanner object to traverse each line.
             Scanner csvLine = new Scanner(csvReader.nextLine());
             csvLine.useDelimiter(",");
 
-            // Define first cell of each line as identifier. Will either be firm abbreviation OR ticker.
+            // Define first cell of each line as identifier. Will either be firm abbreviation or date.
             String identifier = csvLine.next();
 
-            //TODO: Determine why identifier length is different.
+            // TODO: Determine why identifier length is different.
             //If identifier is firm abbreviation...
             if (identifier.length() == 3 || identifier.length() == 2) {
+                System.out.println(identifier.length());
                 while (csvLine.hasNext()) {
-                    // Create stock object and add to list.
+                    // TODO: Determine if object already exists.
+                    // Create stock objects and add to list.
                     tempTickers.add(new StockImpl("src/analysis/data/" + csvLine.next() + ".csv"));
                 }
-                s = 0;
-                // Otherwise... traverse row and perform evaluation.
-            } else {
-                // TODO: Control needs to be assigned from first given date.
-                // TODO: 0% row needs to be skipped.
-                if (identifier.equals("2019-01-14")) {
-                    controlDate = identifier;
-                }
+                n = 0;
 
+                // If on 0% control row, set control date and move to next row.
+            } else if (n == 1) {
+                controlDate = identifier;
+
+                // Otherwise, iterate through each cell and compare to predicted value.
+            } else {
                 while(csvLine.hasNext()) {
                     if (i == 0) {
                         workingDate = identifier;
                     }
                     // If performance > predicted.
+                    //TODO: Add error messages for missing file / date.
+                    //TODO: Automatically find nearest date.
                     if (stockPerformance(controlDate, workingDate, tempTickers.get(i)) >= Double.parseDouble(csvLine.next())) {
                         System.out.println("Growth exceeds or meets prediction from " + controlDate + " to " + workingDate + ".");
                     } else {
@@ -71,7 +69,7 @@ public class Analytics {
             }
             i = 0;
             csvLine.close();
-            s ++;
+            n ++;
         }
         csvReader.close();
     }
